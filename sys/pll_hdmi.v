fc5ffac1b487255906e9eeaca040e6b9987001d8
birdybro <kcoleman@misterfpga.co>
2025-09-23 18:13:45 -0600

    Fix device model name in pll files

--- sys/pll_hdmi.v
+++ sys/pll_hdmi.v
@@ -53,7 +53,7 @@ endmodule
 // Retrieval info: 	<generic name="debug_print_output" value="false" />
 // Retrieval info: 	<generic name="debug_use_rbc_taf_method" value="false" />
 // Retrieval info: 	<generic name="device_family" value="Cyclone V" />
-// Retrieval info: 	<generic name="device" value="5CEBA2F17A7" />
+// Retrieval info: 	<generic name="device" value="5CSEBA6U23I7" />
 // Retrieval info: 	<generic name="gui_device_speed_grade" value="2" />
 // Retrieval info: 	<generic name="gui_pll_mode" value="Fractional-N PLL" />
 // Retrieval info: 	<generic name="gui_reference_clock_frequency" value="50.0" />
